3feadd46aaad029b9e4edb1af250ce8f22089421
Tanjir-Hasan <ihasan.tanjir@gmail.com>
2023-04-06 03:57:41 +0600

    final so far

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,21 +2,22 @@ import React, { useState } from 'react';
 import { BookOpenIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
 import { BarsArrowDownIcon, BarsArrowUpIcon } from '@heroicons/react/24/solid';
+import ActiveLink from './ActiveLink';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <nav>
-            <div className='relative text-xl font-medium text-blue-700 bg-slate-200 max-w-7xl mx-auto my-3 px-4 py-4 h-16 rounded-xl shadow-lg'>
+            <div className='relative text-xl font-medium bg-slate-200 max-w-7xl mx-auto my-3 px-4 py-4 h-16 rounded-xl shadow-lg'>
                 <div className='flex justify-between'>
                     <div className='flex'>
                         <BookOpenIcon className='lg:flex h-8 w-6 text-blue-500' />
-                        <h3 className='ml-2 text-xl lg:text-2xl'>Bibliophile's Corner</h3>
+                        <Link to='/' className='ml-2 text-xl lg:text-2xl text-blue-700'>Bibliophile's Corner</Link>
                     </div>
-                    <div className='hidden lg:block'>
-                        <Link to='/' className='mr-4'>Home</Link>
-                        <Link to='/books' className='mr-4'>Shop</Link>
-                        <Link to='/about' className='mr-4'>About</Link>
+                    <div className='hidden gap-3 lg:block'>
+                        <ActiveLink to='/'>Home</ActiveLink>
+                        <ActiveLink to='/books'>Shop</ActiveLink>
+                        <ActiveLink to='/about'>About</ActiveLink>
                     </div>
                 </div>                
             </div>
